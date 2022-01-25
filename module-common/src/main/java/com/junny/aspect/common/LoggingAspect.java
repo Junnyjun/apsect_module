62774de0c42411eddd8dc4c881c32e233c4bbc2e
Junnyjun <chbe5082@naver.com>
2022-01-25 23:37:57 +0900

    first commit

--- module-common/src/main/java/com/junny/aspect/common/LoggingAspect.java
+++ module-common/src/main/java/com/junny/aspect/common/LoggingAspect.java
@@ -1,22 +1,10 @@
 package com.junny.aspect.common;
 
-import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static java.util.Optional.ofNullable;
 
 @Slf4j
 @Component
@@ -34,26 +22,6 @@ public class LoggingAspect {
         return proceedingJoinPoint.proceed();
     }
 
-    @Around("within(com.junny.common.*))")
-    public Object logging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        Object result;
-        LoggingModel loggingModel = loggingInitData(proceedingJoinPoint);
-        String className = loggingModel.getClassName();
-        String methodName = loggingModel.getMethodName();
-        log.info(">>>>>> Logging start");
-        log.info("URI          : {}", loggingModel.getUri());
-        log.info("IP           : {}", loggingModel.getIp());
-        log.info("PARAMS       : {}", loggingModel.getParams());
-        log.info(">> {}.{}() process start -----", className, methodName);
-        result = mainProcess(proceedingJoinPoint);
-        log.info("<< {}.{}() process end   -----", className, methodName);
-        log.info("PROCESS TIME : ({}s)", processTime);
-        log.info("RESULT       : {}", result);
-        log.info("<<<<<< Logging end");
-
-        return result;
-    }
-
     private Object mainProcess(ProceedingJoinPoint pjp) throws Throwable {
         long startAt = System.currentTimeMillis();
         Object result = pjp.proceed();
@@ -62,42 +30,7 @@ public class LoggingAspect {
 
         return result;
     }
-    private LoggingModel loggingInitData(ProceedingJoinPoint pjp) {
-        String params = "X";
-        String uri = "X";
-        String ip = "X";
-        RequestAttributes requestAttributes = RequestContextHolder
-                .getRequestAttributes();
-        if (requestAttributes != null) {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-                    .getRequestAttributes()).getRequest();
-            Map<String, String[]> paramMap = request.getParameterMap();
-            if (!paramMap.isEmpty()) {
-                params = " [" + paramMapToString(paramMap) + "]";
-            }
-            uri = request.getRequestURI();
 
-            ip = ofNullable(request.getHeader("X-FORWARDED-FOR"))
-                    .orElse(request.getRemoteAddr());
-        }
-        String className = pjp.getSignature().getDeclaringTypeName();
-        String methodName = pjp.getSignature().getName();
-
-        return LoggingModel.builder()
-                .params(params)
-                .uri(uri)
-                .ip(ip)
-                .className(className)
-                .methodName(methodName)
-                .build();
-    }
-
-    private String paramMapToString(Map<String, String[]> paramMap) {
-        return paramMap.entrySet().stream()
-                .map(entry -> String.format("%s -> (%s)",
-                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
-                .collect(Collectors.joining(", "));
-    }
 
 
 
